1c49bf3bcaa61a889780d6d07a798381b6f9a1ea
Irina Adamuk <grinirina032@gmail.com>
2022-06-27 21:17:16 +0300

    error case in thunk was tested

--- src/store/__tests/gists/thunks.test.js
+++ src/store/__tests/gists/thunks.test.js
@@ -1,4 +1,4 @@
-import { getGists, getGistsStart, getGistsSuccess, getGistsByNameStart, getGistsByNameSuccess, getGistsByName } from "../../gists";
+import { getGists, getGistsStart, getGistsSuccess, getGistsError, getGistsByNameStart, getGistsByNameSuccess, getGistsByName, getGistsByNameError } from "../../gists";
 
 describe("get gists thunk", () => {
   it("success", async () => {
@@ -19,6 +19,26 @@ describe("get gists thunk", () => {
 		expect(dispatch).toBeCalledTimes(2);
 		expect(dispatch).toHaveBeenNthCalledWith(1, getGistsStart());
 		expect(dispatch).toHaveBeenNthCalledWith(2, getGistsSuccess(DATA));
+	});
+
+	it("error", async () => {
+		const PAGE = 5
+		const ERROR = {error: "error"}
+
+		const dispatch = jest.fn()
+		const getPublicGistsApi = jest.fn().mockRejectedValue(ERROR);
+
+
+		const thunk = getGists(PAGE);
+
+		await thunk(dispatch, null, { getPublicGistsApi });
+
+		expect(getPublicGistsApi).toBeCalledWith(PAGE);
+		expect(getPublicGistsApi).toBeCalledTimes(1);
+
+		expect(dispatch).toBeCalledTimes(2);
+		expect(dispatch).toHaveBeenNthCalledWith(1, getGistsStart());
+		expect(dispatch).toHaveBeenNthCalledWith(2, getGistsError(ERROR));
 	});
 });
 
@@ -42,5 +62,25 @@ describe("get gists by name thunk", () => {
 		expect(dispatch).toBeCalledTimes(2);
 		expect(dispatch).toHaveBeenNthCalledWith(1, getGistsByNameStart());
 		expect(dispatch).toHaveBeenNthCalledWith(2, getGistsByNameSuccess(DATA));
+	});
+
+	it("error", async () => {
+		const NAME = "bogdanq"
+		const ERROR = {error: "error"}
+
+		const dispatch = jest.fn()
+		const getGistsByNameApi = jest.fn().mockRejectedValue(ERROR);
+
+
+		const thunk = getGistsByName(NAME);
+
+		await thunk(dispatch, null, { getGistsByNameApi });
+
+		expect(getGistsByNameApi).toBeCalledWith(NAME);
+		expect(getGistsByNameApi).toBeCalledTimes(1);
+
+		expect(dispatch).toBeCalledTimes(2);
+		expect(dispatch).toHaveBeenNthCalledWith(1, getGistsByNameStart());
+		expect(dispatch).toHaveBeenNthCalledWith(2, getGistsByNameError(ERROR));
 	});
 });
